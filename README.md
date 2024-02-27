b0cd1699276d636767aa220a9940d34ca7232d4a
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-27 07:35:16 +0000

    make a whole scaffold

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ruby-challenge
+# ruby-scriting-challenge
---
+++ generated_sql/dummy.sql
@@ -0,0 +1,28 @@
+-- table1
+insert into table1() values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),;
+
+insert into table1() values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),;
+
+insert into table1() values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),
+                        values();
+
+insert into table1() values(),
+                        values(),
+                        values(),
+                        values(),
+                        values(),
+                        values();

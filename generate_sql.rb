85b4c826a032fa07f8298558191119176553deaf
ryota-weapon <61468664+ryota-weapon@users.noreply.github.com>
2024-02-27 12:16:42 +0000

    refactor

--- generate_sql.rb
+++ generate_sql.rb
@@ -1,4 +1,5 @@
 require "yaml"
+require 'faker'
 
 begin
     yaml_file_path = ARGV[0]
@@ -12,13 +13,32 @@ end
 
 
 if !data.include?("Tables") or data["Tables"].empty?
-    puts "Write tables info to insert columns"
+    puts "Write tables info to insert records"
     puts "Program ended without generating a sql file."
     exit
 end
 
-def new_column_data(column, index)
-    "hoge"
+def new_column_data(column, option, index)
+    case column[0].downcase
+    when "id"
+        index
+    when "name"
+        Faker::Name.name
+    when "email"
+        Faker::Internet.unique.email
+    when "date"
+        Faker::Date.birthday
+    when "content"
+        Faker::Lorem.sentence
+    else
+        "hoge"
+    end
+end
+
+def make_sql_command(sql_base, values)
+    cut = ",\n".length
+    values = values[0...-cut] 
+    new_sql = sql_base + values + ";\n"
 end
 
 def create_bulk_insert_sql(data, all_records, bulk_number)
@@ -34,16 +54,15 @@ def create_bulk_insert_sql(data, all_records, bulk_number)
             sql_bulk_values += "(#{record.join(", ")}),\n"
             cnt += 1
             if cnt == bulk_number
-                cut = ",\n".length
-                sql_bulk_values = sql_bulk_values[0...-cut] 
-                # sql_bulk_values = sql_bulk_values.slice!(-3,3)
-                new_sql = sql_base + sql_bulk_values + ";\n"
-                sqls << new_sql
+                sqls << make_sql_command(sql_base, sql_bulk_values)
                 # initialize
                 sql_bulk_values = ""
                 cnt = 0
             end
         end
+        if sql_bulk_values!=""
+            sqls << make_sql_command(sql_base, sql_bulk_values)
+        end           
         res[table] = sqls
     end
 
@@ -55,13 +74,12 @@ all_records = {}
 data["Tables"].each do |table_name, table_info|
     column_count = table_info["count"]
     column_info = table_info["columns"]
-
     column_names = column_info.map(&:keys)
     records = []
-    0.upto(column_count-1) do |index|
+    0.upto(column_count) do |index|
         new_record = []
         column_names.each do |column|
-            new_record << (new_column_data(column, index))
+            new_record << (new_column_data(column, "option", index))
         end
         records << new_record
     end
@@ -70,7 +88,7 @@ end
 
 sql_commands_hash = create_bulk_insert_sql(data, all_records, bulk_number)
 
-#generate a file
+# #generate a file
 File.open("./generated_sql/result#{Time.now}.sql", "w") do |file|
     sql_commands_hash.each do |table, commands|
         file.puts %Q!--insert for table "#{table}"!
